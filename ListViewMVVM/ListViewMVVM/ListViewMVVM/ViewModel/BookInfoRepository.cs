7d5c36c1d2caf80b2fb47d1d3b705e6201b1bd09
Gnana Priya N <gnanapriya.namasivayam@syncfusion.com>
2019-08-05 09:18:25 +0530

    Uploaded sample and github

---
+++ ListViewMVVM/ListViewMVVM/ListViewMVVM/ViewModel/BookInfoRepository.cs
@@ -0,0 +1,66 @@
+﻿using Syncfusion.ListView.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MVVM
+{
+public class BookInfoRepository : INotifyPropertyChanged
+{
+    private ObservableCollection<BookInfo> bookInfoCollection;
+    public event PropertyChangedEventHandler PropertyChanged;
+    private Command<ItemSelectionChangingEventArgs> selectedItem;
+
+    public ObservableCollection<BookInfo> BookInfoCollection
+    {
+        get { return bookInfoCollection; }
+        set
+        {
+            this.bookInfoCollection = value;
+            this.OnPropertyChanged("BookInfoCollection");
+        }
+    }
+
+    public Command<ItemSelectionChangingEventArgs> SelectedItem
+    {
+        get { return this.selectedItem; }
+        set
+        {
+            this.selectedItem = value;
+            this.OnPropertyChanged("SelectedItem");
+        }
+    }
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+    public BookInfoRepository()
+    {
+        GenerateNewBookInfo();
+        selectedItem = new Command<ItemSelectionChangingEventArgs>(OnSelectionChanging);
+    }
+
+        private void OnSelectionChanging(ItemSelectionChangingEventArgs obj)
+        {
+            var eventArgs = obj as ItemSelectionChangingEventArgs;
+            if (eventArgs.AddedItems.Count > 0 && eventArgs.AddedItems[0] == this.BookInfoCollection[0])
+                eventArgs.Cancel = true;
+        }
+
+        private void GenerateNewBookInfo()
+        {
+            BookInfoCollection = new ObservableCollection<BookInfo>();
+            BookInfoCollection.Add(new BookInfo() { BookName = "Machine Learning Using C#", BookDescription = "You’ll learn several different approaches to applying machine learning"});
+            BookInfoCollection.Add(new BookInfo() { BookName = "Object-Oriented Programming in C#", BookDescription = "Object-oriented programming is the de facto programming paradigm"});
+            BookInfoCollection.Add(new BookInfo() { BookName = "C# Code Contracts", BookDescription = "Code Contracts provide a way to convey code assumptions"});
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# Xamarin-forms-listview-selected-item-binding-selectionchanging-event
-Xamarin forms listview selected item binding selectionchanging event
+# Binding SelectionChanging event to Listview
+
+In ListView, the [SelectionChanging](https://help.syncfusion.com/cr/cref_files/xamarin/Syncfusion.SfListView.XForms~Syncfusion.ListView.XForms.SfListView~SelectionChanging_EV.html) event will be raised when selecting an item at the execution time. MVVM for the `SelectionChanging` event can be achieved by binding through the event to command converter.
+
+Refer [EventToCommand](https://www.syncfusion.com/kb/7523/how-to-turn-events-into-commands-with-behaviors-in-sflistview?_ga=2.49538854.1823797169.1563166718-1085055173.1562420655) knowledge base to create the command for event using behavior.
+
+```
+<syncfusion:SfListView x:Name="listView" 
+                       ItemsSource="{Binding BookInfoCollection}">
+    <syncfusion:SfListView.Behaviors>
+        <local:EventToCommandBehavior EventName="SelectionChanging" Command="{Binding SelectedItem}" 
+                                          Converter="{StaticResource EventArgs}"/>
+    </syncfusion:SfListView.Behaviors>
+</syncfusion:SfListView>
+```
+```
+//ViewModel.cs
+public class BookInfoRepository : INotifyPropertyChanged
+{
+    private Command<ItemSelectionChangingEventArgs> selectedItem
+
+    public Command<ItemSelectionChangingEventArgs> SelectedItem
+    {
+        get { return this.selectedItem; }
+        set
+        {
+            this.selectedItem = value;
+            this.OnPropertyChanged("SelectedItem");
+        }
+    }
+
+    public BookInfoRepository()
+    {
+        selectedItem = new Command<ItemSelectionChangingEventArgs>(OnSelectionChanging);
+    }
+
+    ///<summary>
+    ///To disable the selection for particular item
+    ///</summary>
+    public void OnSelectionChanging(ItemSelectionChangingEventArgs obj)
+    {
+        var eventArgs = obj as ItemSelectionChangingEventArgs;
+        if (eventArgs.AddedItems.Count > 0 && eventArgs.AddedItems[0] == this.BookInfoCollection[0])
+            eventArgs.Cancel = true;
+    }
+}
+```
+To know more about MVVM in ListView, please refer our documentation [here](https://help.syncfusion.com/xamarin/sflistview/mvvm)
